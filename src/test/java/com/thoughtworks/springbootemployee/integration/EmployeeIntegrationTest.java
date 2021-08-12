7886e29ef77c59cd22563529a8d4eb56227d123b
balajma <mark.lorenz.balajadia@oocl.com>
2021-08-13 00:57:06 +0800

    [SpringBootAPI][Mark] test: Added integration test for add employee -should_add_employees_when_add_employee()

--- src/test/java/com/thoughtworks/springbootemployee/integration/EmployeeIntegrationTest.java
+++ src/test/java/com/thoughtworks/springbootemployee/integration/EmployeeIntegrationTest.java
@@ -1,17 +1,16 @@
 package com.thoughtworks.springbootemployee.integration;
 
-import com.thoughtworks.springbootemployee.model.Employee;
 import com.thoughtworks.springbootemployee.repository.EmployeeRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-import java.util.Optional;
-
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -38,7 +37,8 @@ public class EmployeeIntegrationTest {
                 .andExpect(jsonPath("$[2].gender").value("female"));
     }
 
-    @Test public void should_return_employee_when_get_employee_given_employee_id() throws Exception {
+    @Test
+    public void should_return_employee_when_get_employee_given_employee_id() throws Exception {
         //given
         int id = 1;
         //when
@@ -50,7 +50,8 @@ public class EmployeeIntegrationTest {
                 .andExpect(jsonPath("$.gender").value("male"));
     }
 
-    @Test public void should_return_employees_when_get_employee_by_gender_given_employee_gender() throws Exception {
+    @Test
+    public void should_return_employees_when_get_employee_by_gender_given_employee_gender() throws Exception {
         //given
         String gender = "male";
         //when
@@ -65,23 +66,39 @@ public class EmployeeIntegrationTest {
                 .andExpect(jsonPath("$[1].gender").value("male"));
     }
 
-    @Test public void should_return_employees_when_get_employee_by_pagination_given_page_index_and_size() throws Exception {
+    @Test
+    public void should_return_employees_when_get_employee_by_pagination_given_page_index_and_size() throws Exception {
         //given
         //when
         //then
         mockMvc.perform(MockMvcRequestBuilders.get("/employees")
-                .param("pageIndex", "1").param("pageSize","3"))
+                .param("pageIndex", "1").param("pageSize", "3"))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$[0].name").value("Gronk"))
                 .andExpect(jsonPath("$[1].name").value("Kees"))
                 .andExpect(jsonPath("$[2].name").value("Linne"));
     }
 
+    @Test
+    public void should_add_employees_when_add_employee() throws Exception {
+        //given
+        String newEmployee = "{\n" +
+                "    \"name\" : \"Mang Goryo\",\n" +
+                "    \"age\" : 101,\n" +
+                "    \"gender\" : \"male\",\n" +
+                "    \"salary\" : \"1\",\n" +
+                "    \"companyid\" : 2\n" +
+                "}";
+        //when
+        //then
+        mockMvc.perform(MockMvcRequestBuilders.post("/employees")
+                .contentType(MediaType.APPLICATION_JSON).content(newEmployee))
+                .andExpect(jsonPath("$.name").value("Mang Goryo"))
+                .andExpect(jsonPath("$.age").value("101"))
+                .andExpect(jsonPath("$.salary").value("1"));
+    }
+
     
-
-
-
-
 
 
 }
