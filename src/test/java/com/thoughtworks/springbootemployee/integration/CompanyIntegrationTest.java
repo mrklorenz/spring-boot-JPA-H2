66867c4534f61ab5d5b6e388b5ceaea69af0583b
balajma <mark.lorenz.balajadia@oocl.com>
2021-08-13 01:33:59 +0800

    [SpringBootAPI][Mark] test: Added integration test for get all companies- should_return_all_companies_when_get_all_companies()

---
+++ src/test/java/com/thoughtworks/springbootemployee/integration/CompanyIntegrationTest.java
@@ -0,0 +1,35 @@
+package com.thoughtworks.springbootemployee.integration;
+
+import com.thoughtworks.springbootemployee.repository.CompanyRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class CompanyIntegrationTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private CompanyRepository companyRepository;
+
+    @Test
+    public void should_return_all_companies_when_get_all_companies() throws Exception {
+        //given
+        //when
+        //then
+        mockMvc.perform(MockMvcRequestBuilders.get("/companies"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].name").value("OOCL"))
+                .andExpect(jsonPath("$[1].name").value("COSCO"))
+                .andExpect(jsonPath("$[2].name").value("MAERSK"));
+
+    }
+}
